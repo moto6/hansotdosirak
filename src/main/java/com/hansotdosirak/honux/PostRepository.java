2544a4a2d2ca24310cdc071196df48c933ccba02
dongdongKing <ultraraptor@naver.com>
2023-02-25 11:38:28 +0900

    feat : gogo

--- src/main/java/com/hansotdosirak/honux/PostRepository.java
+++ src/main/java/com/hansotdosirak/honux/PostRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.LockModeType;
 import java.util.Optional;
@@ -12,6 +13,6 @@ public interface PostRepository extends JpaRepository<Post,Long> {
     Optional<Post> findById(Long postId);
 
     @Modifying
-    @Query(value = "update Post set likeCount = likeCount+1")
-    void updateAtomic(Long postId);
+    @Query(value = "update Post set likeCount = likeCount+1 where id =:postId")
+    void updateAtomic(@Param(value = "postId") Long postId);
 }
