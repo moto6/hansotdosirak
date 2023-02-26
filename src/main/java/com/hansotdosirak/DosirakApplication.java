38fc81f62efecbd0baf65a8eef5b71f49e9527a4
dongdongKing <ultraraptor@naver.com>
2023-02-26 22:23:45 +0900

    feat : change project name

--- src/main/java/com/hansotdosirak/DosirakApplication.java
+++ src/main/java/com/hansotdosirak/DosirakApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HansotdosirakApplication {
+public class DosirakApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HansotdosirakApplication.class, args);
+        SpringApplication.run(DosirakApplication.class, args);
     }
 }
